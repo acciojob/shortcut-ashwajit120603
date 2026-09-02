a5f6eb55c4cbd6ed1e58148cba539cf507d90283
ashwajit120603 <ashwajitsurwade945@gmail.com>
2026-09-02 11:50:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1.length==0 || s2.length==0){
+		return " ";
+	}
+	let res=s1[0]+s2[0];
+
+	return res;
 }
 
 // Do not change the code below.
